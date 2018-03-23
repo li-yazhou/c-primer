0883ee7ed044167aaf15d3c4879f99800ffb3385
lyz0907 <yazhou6543@gmail.com>
2018-03-23 22:31:24 +0800

    the c programming language 2nd, chapter 02

---
+++ the-c-programming-language-2nd/chap01/get_putchar.c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int ch;
+    // 输入的字符会被缓存起来，当按回车键后，刷新缓存中的字符，
+    // 相当于输入一行字符之后显示一行字符
+    while((ch = getchar()) != EOF){
+        putchar(ch);
+    }
+    return 0;
+}
